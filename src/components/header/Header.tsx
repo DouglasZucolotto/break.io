ff6b039174cf97b68333dcce8d84f5765f92f3ad
DouglasZucolotto <douglaszuco2@gmail.com>
2024-04-29 22:59:49 -0300

    first commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import * as S from './style'
+import logo from '../../img/logo.svg'
+
+export const Header = () => {
+  return (
+    <S.Header>
+        <img src={logo} alt="" />
+    </S.Header>
+  )
+}
